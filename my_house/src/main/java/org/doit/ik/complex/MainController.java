d2a6604c0d8dc93c17dcd302afcb344dd6f28787
chacha <hac0309@naver.com>
2026-02-25 16:24:27 +0900

    260225
    layout 나눴음

--- my_house/src/main/java/org/doit/ik/complex/MainController.java
+++ my_house/src/main/java/org/doit/ik/complex/MainController.java
@@ -13,26 +13,26 @@ import org.springframework.web.bind.annotation.PathVariable;
 @Controller
 public class MainController {
 
-	@Value("${kakao.maps.js-key}")
+    @Value("${kakao.maps.js-key}")
     private String kakaoJsKey;
-	
+
     @GetMapping("/main")
     public String main(Model model, Principal principal) {
         model.addAttribute("isLogin", principal != null);
-
-        // 일단 화면만 띄우려면 houses 없어도 됨(템플릿에 더미가 뜸)
-        // model.addAttribute("houses", List.of()); // 있어도 되고 없어도 됨
-
         model.addAttribute("kakaoJsKey", kakaoJsKey);
-        model.addAttribute("alarmCount", 3); // 없으면 배지 안뜸
-        return "/main/main";
+        model.addAttribute("alarmCount", 3);
+        return "main/main";
     }
-    
+
     @GetMapping("/listing/{propertyKey}")
-    public String detail(@PathVariable("propertyKey") String propertyKey, Model model) {
+    public String detail(@PathVariable("propertyKey") String propertyKey,
+                         Model model,
+                         Principal principal) {
+
+        model.addAttribute("isLogin", principal != null);
+        model.addAttribute("kakaoJsKey", kakaoJsKey);
 
         Map<String, Object> property = new HashMap<>();
-
         property.put("name", "서울 관악구 신림동 원룸");
         property.put("price", "보증금 1000 / 월세 45");
         property.put("type", "원룸");
@@ -40,13 +40,9 @@ public class MainController {
         property.put("address", "서울특별시 관악구 신림동 123-45");
         property.put("lat", 37.501824);
         property.put("lng", 126.9476234);
-
-        // 치안 더미
         property.put("safetyScore", 78);
         property.put("cctvCount", 21);
         property.put("safeRoadDistance", 320);
-        
-        model.addAttribute("kakaoJsKey", kakaoJsKey);
 
         model.addAttribute("property", property);
 
